a6e54ede074bd0fa7ffe73a273e98bdd98d6f4a0
whgds1360 <maxprizrac@mail.ru>
2026-02-22 21:27:52 +0300

    UPDATE КС ЛАБА3; МДК 05.02 ЛАБА 16

--- МДК 05.02/ЛАБА 16/record_toy.pas
+++ МДК 05.02/ЛАБА 16/record_toy.pas
@@ -1,13 +1,13 @@
 ﻿type
   Toy = record
     public
-    name: string;
+    name: string[50];
     price: integer;
-    age: string;
+    age: string[50];
   end;
   
-  begin
-    var my_toy:Toy;
+begin
+    var my_toy: Toy;
     
     with my_toy do
     begin
@@ -16,5 +16,25 @@
       age := '5-17';
     end;
     
-    Print($'Наименование: {my_toy.name} {#10}Цена: {my_toy.price}руб. {#10}Возраст: {my_toy.age}')
-  end.
+    Print($'Наименование: {my_toy.name} {#10}Цена: {my_toy.price}руб. {#10}Возраст: {my_toy.age}');
+    //////////////////////////////////////////////////////////////////////////////////////////////Задание 1
+    var f: file of Toy;
+    
+    Assign(f, 'record_toy.txt');
+    Rewrite(f);
+    
+    write(f, my_toy); 
+    Close(f);         
+    ////////////////////////////
+    Reset(f);  
+
+    var content: Toy;  
+    
+    while not Eof(f) do 
+    begin  
+      Read(f, content);  
+      
+      Print($'{#10}Наименование: {content.name} {#10}Цена: {content.price}руб. {#10}Возраст: {content.age}');
+    end;  
+    Close(f);
+end.
