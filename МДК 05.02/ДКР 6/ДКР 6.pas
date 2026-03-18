1033582f01663d58da72d54ffffd92f74766a24c
whgds1360 <maxprizrac@mail.ru>
2026-03-18 10:39:24 +0300

    Update МДК 05.02 ДКР 6

--- МДК 05.02/ДКР 6/ДКР 6.pas
+++ МДК 05.02/ДКР 6/ДКР 6.pas
@@ -1,130 +1,105 @@
 ﻿const
-  SIZE = 10;
+  SIZE = 3;
 
-type
-  DEK = record
-    data: array[1..SIZE] of integer;
-    head: integer;
-    tail: integer;
-    count: integer;
-  end;
+type DEK = record
+  data: array[0..SIZE] of integer;
+  head, tail, count: integer;
+end;
 
 procedure Init(var d: DEK);
 begin
-  d.head := 1;
-  d.tail := SIZE;
+  d.head := 0;
+  d.tail := 0;
   d.count := 0;
 end;
 
-function IsEmpty(var d: DEK): boolean;
-begin
-  IsEmpty := d.count = 0;
-end;
+function IsEmpty(var d: DEK): boolean := d.count = 0;
 
-function IsFull(var d: DEK): boolean;
-begin
-  IsFull := d.count = SIZE;
-end;
+function IsFull(var d: DEK): boolean := d.count = SIZE;
 
 procedure Push(var d: DEK; place: string; number: integer);
 begin
-  if IsFull(d) then
-  begin
-    writeln('Дек переполнен!');
+  if IsFull(d) then begin
+    println('Дек переполнен!');
     exit;
   end;
-  
-  case place of
-    'H': begin
-      d.head := d.head - 1;
-      if d.head < 1 then
-        d.head := SIZE;
-      d.data[d.head] := number;
-    end;
+
+  if place = 'H' then begin
+    d.head := d.head - 1;
+    if d.head < 0 then d.head := SIZE;
+    d.data[d.head] := number;
     
-    'T': begin
-      d.tail := d.tail + 1;
-      if d.tail > SIZE then
-        d.tail := 1;
-      d.data[d.tail] := number;
-    end;
+  end else if place = 'T' then begin
+    d.data[d.tail] := number;
+    d.tail := d.tail + 1;
+    if d.tail > SIZE then d.tail := 0;
+    
   end;
-  
+
   d.count := d.count + 1;
 end;
 
 procedure ShowDek(var d: DEK);
-var
-  i, pos: integer;
 begin
-  writeln;
-  writeln('Содержимое дека:');
+  println;
+  println('Содержимое дека:');
   
   if IsEmpty(d) then
-    writeln('[пусто]')
-  else
-  begin
-    write('[ ');
-    pos := d.head;
-    for i := 1 to d.count do
-    begin
-      write(d.data[pos], ' ');
+    println('[пусто]')
+  else begin
+    var pos := d.head;
+    print('[ ');
+    for var i := 1 to d.count do begin
+      print(d.data[pos], ' ');
       pos := pos + 1;
-      if pos > SIZE then
-        pos := 1;
+      if pos > SIZE then pos := 0;
     end;
-    writeln(']');
+    println(']');
   end;
-  
+
   writeln('Всего элементов: ', d.count);
-  writeln('head (начало) = ', d.head);
-  writeln('tail (конец) = ', d.tail);
+  writeln('head = ', d.head, '  tail = ', d.tail);
   
-  write('Память: [ ');
-  for i := 1 to SIZE do
-    write(d.data[i], ' ');
-  writeln(']');
-  writeln;
+  print('Память: [ ');
+  for var i := 0 to SIZE do print(d.data[i], ' ');
+  println(']');
 end;
 
 
 Begin
   var deker: DEK;
-  var choice, num: integer;
   
   Init(deker);
   
+  println('1 - Добавить в начало');
+  println('2 - Добавить в конец');
+  println('3 - Показать дек');
+  println('0 - Выход');
+  
+  var choice := readinteger();
+  
   repeat
-    writeln('1 - Добавить в начало');
-    writeln('2 - Добавить в конец');
-    writeln('3 - Показать дек');
-    writeln('0 - Выход');
-    writeln('');
-    write('Ваш выбор: ');
-    readln(choice);
-    
     case choice of
       1: begin
-        write('Введите число: ');
-        readln(num);
+        var num:= readinteger('Введите число: ');
         Push(deker, 'H', num);
         ShowDek(deker);
       end;
       
       2: begin
-        write('Введите число: ');
-        readln(num);
+        var num:= readinteger('Введите число: ');
         Push(deker, 'T', num);
         ShowDek(deker);
       end;
       
       3: ShowDek(deker);
       
-      0: writeln('Программа завершена');
+      0: println('На этом всё!');
       
       else writeln('Неверный выбор!');
     end;
     
-    writeln;
+    println;
+    choice := readinteger();
   until choice = 0;
 End.
